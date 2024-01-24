b63358be46b6397723c92b3b3174a3e873c085a5
Hariharan <105870478+Hariharan52@users.noreply.github.com>
2024-01-24 22:08:03 +0530

    Create Question2.java

---
+++ 24-01-2024/Question2.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    public static int gameWithCells(int n, int m) 
+    {
+        int squaresInRow = (n + 1) / 2;
+        int squaresInColumn = (m + 1) / 2;
+        int totalSquares = squaresInRow * squaresInColumn;
+
+        return totalSquares;
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int m = Integer.parseInt(firstMultipleInput[1]);
+
+        int result = Result.gameWithCells(n, m);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
